2c33ff66eca0bd95becb3c9da0fa28c5f72b5169
mehdi-khallaghi <mkh.computer@gmail.com>
2024-07-14 23:12:17 +0330

    to fallback in the suspense

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 const Movie = lazy(() => import("./components/movie/movieComponent"));
 const RCounter = lazy(() => import("./components/RCounter/reduxCounterComponent"));
 const MovieDetail = lazy(() => import("./components/MovieDetail/movieDetailcomponent"));
 const RcounterClass = lazy(() => import("./components/RcounterClass"));
 const FooterComponent = lazy(() => import("./components/common/footer"));
 const UserComponent = lazy(() => import("./components/user"));
+const Loader = lazy(() => import("./components/spinner/Spinner"));
 
 // import  MovieDetail  from './components/MovieDetail/movieDetailcomponent';
 // import RCounter from './components/RCounter/reduxCounterComponent';
@@ -13,12 +15,12 @@ const UserComponent = lazy(() => import("./components/user"));
 // import FooterComponent from './components/common/footer';
 // import Movie from './components/movie/movieComponent'
 // import UserComponent from './components/user';
-
+// import Loader from './components/spinner/Spinner';
 
 function App() {
   return (
     <div className="App">
-      <Suspense fallback={<p>loading...please wait!!!</p>}>
+      <Suspense fallback={<Loader/>}>
         <BrowserRouter>
           <Routes>
             <Route path="/" exact element={<Movie />} />
